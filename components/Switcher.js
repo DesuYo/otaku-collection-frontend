66dd2a3923359cb3e4e4158bc1e7ec11f13bc375
Alex <zaychenko96y@outlook.com>
2019-04-14 04:46:42 +0300

    Added button

--- components/Switcher.js
+++ components/Switcher.js
@@ -1,4 +1,24 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  font: bold 14px / 30px "Myriad Pro", sans-serif;
+  color: rgb(134, 50, 170);
+  cursor: pointer;
+
+  &:after {
+    content: '';
+    display: inline-block;
+    position: absolute;
+    left: calc(50% + 75px); right: -25px;
+    top: 8px; bottom: 8px;
+    background: center / contain no-repeat;
+    background-image: url(../images/arrow.png);
+  }
+`
 
 export class AwesomeSwitcher extends Component {
   constructor() {
@@ -16,14 +36,19 @@ export class AwesomeSwitcher extends Component {
     })
   }
 
+
+
   render() {
     const children = React.Children.toArray(this.props.children).map(node => {
       return React.cloneElement(node)
     })
 
     return (
-      <div onClick={ this.handleSwitch }>
+      <div>
         { children[this.state.position] }
+        <StyledButton onClick={ this.handleSwitch }>
+          { children[this.state.position].type.name }
+        </StyledButton>
       </div>
     )
   }
